08cd66bca344758f71c570b1fb084708302a70ea
Skevafinance <cikurraka632@gmail.com>
2026-07-04 10:27:00 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import MarketBoard from "@/components/MarketBoard";
+import EscrowOverview from "@/components/EscrowOverview";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <MarketBoard />
+      <EscrowOverview />
+    </>
+  );
+}
